778b78cdeb254737907206406e26d7cd176fb11d
MustafUmar <u.mustapha.m@gmail.com>
2018-10-06 05:20:13 +0100

    First commit

---
+++ src/java/dao/UserDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author MustiU
+ */
+@Repository
+@Transactional
+public class UserDAO {
+    
+//    @Autowired
+//    private SessionFactory sessionfactory;
+    
+}
---
+++ web/WEB-INF/views/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : Oct 5, 2018, 9:57:16 AM
+    Author     : MustiU
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Buy and Sell Cars</title>
+    </head>
+    <body>
+        <h1>Sell Your Cars</h1>
+        <h1>Buy Amazing Cars</h1>
+    </body>
+</html>
